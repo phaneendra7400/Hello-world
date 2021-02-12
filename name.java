a2d87cfbdc248fc4c813258de5c1c53b3b60e930
phaneendra7400 <phaneendrathatikonda@gmail.com>
2021-02-12 16:46:20 +0530

    name will print 10 times

--- name.java
+++ name.java
@@ -1,5 +1,9 @@
 public class name{
- public static void main (String args[]){
+ public static void main (String args[])
+{ 
+   for (int i=0;i<=10;i++){
+
    System.out.println("java");
+  }
  }
 }
