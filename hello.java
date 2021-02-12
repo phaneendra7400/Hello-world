c6c9510259077b55c4b2b6338565d56585016961
phaneendra7400 <phaneendrathatikonda@gmail.com>
2021-02-12 16:30:03 +0530

    hello world prints 5 times

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
- public static void main (String args[]){
+ public static void main (String args[])
+{
+   for (int i=0,i<=5;i++){
    System.out.println("hello world");
+   }
  }
 }
